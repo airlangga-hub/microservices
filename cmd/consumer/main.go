9ea34c05d2ee04705942af3212690e888227d7f4
Airlangga <airlanggakrishna@gmail.com>
2025-12-02 22:44:08 +0700

    fixed somethings

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/airlangga-hub/microservices/internal"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 	
+	godotenv.Load()
+	
+	password := os.Getenv("PASSWORD")
+	username := os.Getenv("USERNAME")
+	host := os.Getenv("HOST")
+	vhost := os.Getenv("VHOST")
+	
+	conn, err := internal.ConnectRabbit(username, password, host, vhost)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	defer conn.Close()
+	
+	rabbitClient, err := internal.NewRabbitClient(conn)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	defer rabbitClient.Close()
+	
+	messageBus, err := rabbitClient.Consume("customers_created", "email_service", false)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	blocking := make(chan bool)
+	
+	go func() {
+		for message := range messageBus {
+			log.Println("New message: ", message)
+			
+			if err := message.Ack(false); err != nil {
+				log.Println("Acknowledge message failed: ", err)
+				continue
+			}
+			
+			log.Println("Acknowledged message: ", message.MessageId)
+		}
+	}()
+
+	<-blocking
 }
