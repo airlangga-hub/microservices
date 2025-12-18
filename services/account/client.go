e98670ea219db534ad87c228daf18149100236cd
Airlangga <airlanggakrishna@gmail.com>
2025-12-18 16:41:36 +0700

    fix: return type in grpc client from pb struct to account struct

--- services/account/client.go
+++ services/account/client.go
@@ -37,32 +37,38 @@ func NewClient(url string) (*Client, error) {
 	}, nil
 }
 
-func (c *Client) PostAccount(ctx context.Context, name string) (*pb.Account, error) {
+func (c *Client) PostAccount(ctx context.Context, name string) (Account, error) {
 	res, err := c.Service.PostAccount(ctx, &pb.PostAccountRequest{Name: name})
 	if err != nil {
 		log.Println("ERROR: account client PostAccount: ", err)
-		return nil, errors.New("error client post account")
+		return Account{}, errors.New("error client post account")
 	}
 
-	return res.Account, nil
+	return Account{ID: res.Account.Id, Name: res.Account.Name}, nil
 }
 
-func (c *Client) GetAccount(ctx context.Context, id int32) (*pb.Account, error) {
+func (c *Client) GetAccount(ctx context.Context, id int32) (Account, error) {
 	res, err := c.Service.GetAccount(ctx, &pb.GetAccountRequest{Id: id})
 	if err != nil {
 		log.Println("ERROR: account client GetAccount: ", err)
-		return nil, errors.New("error client get account")
+		return Account{}, errors.New("error client get account")
 	}
 	
-	return res.Account, nil
+	return Account{ID: res.Account.Id, Name: res.Account.Name}, nil
 }
 
-func (c *Client) GetAccounts(ctx context.Context, offset, limit int32) ([]*pb.Account, error) {
+func (c *Client) GetAccounts(ctx context.Context, offset, limit int32) ([]Account, error) {
 	res, err := c.Service.GetAccounts(ctx, &pb.GetAccountsRequest{Offset: offset, Limit: limit})
 	if err != nil {
 		log.Println("ERROR: account client GetAccounts: ", err)
 		return nil, errors.New("error client get accounts")
 	}
 	
-	return res.Accounts, nil
+	accounts := []Account{}
+	
+	for _, account := range res.Accounts {
+		accounts = append(accounts, Account{ID: account.Id, Name: account.Name})
+	}
+	
+	return accounts, nil
 }
