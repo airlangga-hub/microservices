d8ecabb534d2cbba564a7d5e37f65cf53f564dd7
Airlangga <airlanggakrishna@gmail.com>
2025-12-26 02:28:10 +0700

    chore: scaffolding order service

--- services/order/service.go
+++ services/order/service.go
@@ -1,6 +1,16 @@
 package order
 
+import "context"
+
 type Service interface {
-	PostOrder()
-	GetOrdersByAccountID()
+	PostOrder(ctx context.Context, accountID int32, products []OrderedProduct) (Order, error)
+	GetOrdersByAccountID(ctx context.Context, accountID int32) ([]*Order, error)
 }
+
+type service struct {
+	repository Repository
+}
+
+func (s *service) PostOrder(ctx context.Context, accountID int32, products []OrderedProduct) (Order, error)
+
+func (s *service) GetOrdersByAccountID(ctx context.Context, accountID int32) ([]*Order, error)
