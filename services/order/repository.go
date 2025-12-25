47999f1b1069e955357bb829cdebb58951f92da4
Airlangga <airlanggakrishna@gmail.com>
2025-12-25 23:00:42 +0700

    refactor: order repo CreateOrder

--- services/order/repository.go
+++ services/order/repository.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 type OrderedProduct struct {
@@ -60,17 +61,51 @@ func (r *repository) Close() error {
 }
 
 func (r *repository) CreateOrder(ctx context.Context, o Order) (Order, error) {
-	if err := r.db.QueryRowContext(
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		log.Println("ERROR: order repo CreateOrder: ", err)
+		return Order{}, errors.New("error initializing db tx")
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	// insert order
+	if err = tx.QueryRowContext(
 		ctx,
 		`INSERT INTO orders (account_id, total_price)
 		VALUES ($1, $2)
-		RETURNING id, account_id, total_price, created_at;`,
+		RETURNING id, created_at;`,
 		o.AccountID, o.TotalPrice,
-	).Scan(&o.ID, &o.AccountID, &o.TotalPrice, &o.CreatedAt); err != nil {
+	).Scan(&o.ID, &o.CreatedAt); err != nil {
 		log.Println("ERROR: order repo CreateOrder: ", err)
-		return Order{}, errors.New("error creating order")
+		return Order{}, fmt.Errorf("error creating order: %v", err)
 	}
 
+	// insert order products
+	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("order_products", "order_id", "product_id", "quantity"))
+
+	for _, p := range o.Products {
+		_, err := stmt.ExecContext(ctx, o.ID, p.ID, p.Quantity)
+		if err != nil {
+			log.Println("ERROR: order repo CreateOrder: ", err)
+			return Order{}, fmt.Errorf("error creating order: %v", err)
+		}
+	}
+
+	_, err = stmt.ExecContext(ctx)
+	if err != nil {
+		log.Println("ERROR: order repo CreateOrder: ", err)
+		return Order{}, fmt.Errorf("error creating order: %v", err)
+	}
+	
+	stmt.Close()
+	
 	return o, nil
 }
 
