539de57736a3289e3141abe20d640bd25e9d6644
Airlangga <airlanggakrishna@gmail.com>
2025-12-26 02:29:19 +0700

    feat: order service NewService

--- services/order/service.go
+++ services/order/service.go
@@ -11,6 +11,10 @@ type service struct {
 	repository Repository
 }
 
-func (s *service) PostOrder(ctx context.Context, accountID int32, products []OrderedProduct) (Order, error)
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+func (s *service) PostOrder(ctx context.Context, accountID int32, products []OrderedProduct) (Order, error) 
 
 func (s *service) GetOrdersByAccountID(ctx context.Context, accountID int32) ([]*Order, error)
