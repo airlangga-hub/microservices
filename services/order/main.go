19bc70efcf92a6ad01b79e23da3ba75978c96ae2
Airlangga <airlanggakrishna@gmail.com>
2025-12-26 23:06:55 +0700

    feat: order main & fix: order server

---
+++ services/order/main.go
@@ -0,0 +1,23 @@
+package order
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	dbUrl := os.Getenv("ORDER_DB_URL")
+	port := os.Getenv("ORDER_PORT")
+	accountServiceUrl := os.Getenv("ACCOUNT_SERVICE_URL")
+	catalogServiceUrl := os.Getenv("CATALOG_SERVICE_URL")
+
+	repository, err := NewRepository(dbUrl)
+	if err != nil {
+		log.Fatalf("ERROR: order main: couldn't create repository: %v", err)
+	}
+	defer repository.Close()
+	
+	service := NewService(repository)
+	
+	log.Fatal(ListenGrpc(service, port, accountServiceUrl, catalogServiceUrl))
+}
--- services/order/server.go
+++ services/order/server.go
@@ -5,16 +5,20 @@ import (
 	"log"
 	"net"
 
+	"github.com/airlangga-hub/microservices/services/account"
+	"github.com/airlangga-hub/microservices/services/catalog"
 	"github.com/airlangga-hub/microservices/services/order/pb"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
 	pb.UnimplementedOrderServiceServer
-	Svc Service
+	Svc           Service
+	AccountClient *account.Client
+	CatalogClient *catalog.Client
 }
 
-func ListenGrpc(service Service, port string) error {
+func ListenGrpc(service Service, port string, accountServiceUrl, catalogServiceUrl string) error {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("ERROR: account server ListenGrpc: %v", err)
