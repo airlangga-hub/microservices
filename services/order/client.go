276157be1c078b524eb518c43dba8161c96f9b60
Airlangga <airlanggakrishna@gmail.com>
2025-12-27 22:31:27 +0700

    feat: order client NewClient

---
+++ services/order/client.go
@@ -0,0 +1,36 @@
+package order
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/airlangga-hub/microservices/services/order/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	Conn    *grpc.ClientConn
+	Service pb.OrderServiceClient
+}
+
+func NewClient() (*Client, error) {
+	target := "localhost" + os.Getenv("ACCOUNT_PORT")
+
+	opts := []grpc.DialOption{}
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.NewClient(target, opts...)
+	if err != nil {
+		log.Fatalf("ERROR: account client NewClient: %v", err)
+		return nil, errors.New("error creating grpc client connection")
+	}
+
+	service := pb.NewOrderServiceClient(conn)
+
+	return &Client{
+		Conn:    conn,
+		Service: service,
+	}, nil
+}
