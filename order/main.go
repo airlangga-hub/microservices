b8ab99f29f11fe76a3ea7bb845c02bff9f4ec6e1
Airlangga <airlanggakrishna@gmail.com>
2026-01-10 10:53:03 +0700

    fix: graceful shutdown

--- order/main.go
+++ order/main.go
@@ -1,8 +1,15 @@
-package order
+package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/airlangga-hub/microservices/order/pb"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -14,8 +21,31 @@ func main() {
 		log.Fatalf("ERROR: order main: couldn't create repository: %v", err)
 	}
 	defer repository.Close()
-	
+
 	service := NewService(repository)
-	
-	log.Fatal(ListenGrpc(service, port))
+
+	s := grpc.NewServer()
+	pb.RegisterOrderServiceServer(s, &Server{Svc: service})
+
+	exitChan := make(chan error, 1)
+
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+		s := <-sig
+		exitChan <- fmt.Errorf("received signal: %v", s)
+	}()
+
+	go func() {
+		lis, _ := net.Listen("tcp", port)
+		err := s.Serve(lis)
+		if err != nil && err != grpc.ErrServerStopped {
+			exitChan <- fmt.Errorf("grpc serve error: %v", err)
+		}
+	}()
+
+	err = <-exitChan
+	log.Printf("Shutting down. Reason: %v", err)
+
+	s.GracefulStop()
 }
