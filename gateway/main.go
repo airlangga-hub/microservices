fd6ec129036dfe47cc60354849da3d3236e7259f
Airlangga <airlanggakrishna@gmail.com>
2026-01-14 10:59:18 +0700

    chore: copy account pb to gateway and begin SignUp

--- gateway/main.go
+++ gateway/main.go
@@ -1,14 +1,18 @@
 package main
 
-import "os"
+import (
+	"net/http"
+	"os"
+)
 
 var jwtSecret []byte
 
 func main() {
 	secret := os.Getenv("JWT_SECRET")
 	jwtSecret = []byte(secret)
-	
+
 	// public endpoints
+	http.HandleFunc("POST /api/signup", SignUp)
 	// buyer endpoints
 	// seller endpoints
 }
